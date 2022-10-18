e5a9d6df2f85db64b6c4b602874a70209192e422
Denis Davydyonok <denisdav06@gmail.com>
2022-10-18 12:22:21 +0300

    I have finished sub-task 1

---
+++ src/main/java/homework/day4/ArraysRunner.java
@@ -0,0 +1,16 @@
+package homework.day4;
+
+public class ArraysRunner {
+
+    public static void main(String[] args) {
+
+        Arrays myArray = new Arrays();
+
+        int[] testArray = {1,2,3,4,5,6,7};
+
+        myArray.sumArray(testArray,3);
+
+
+    }
+
+}
