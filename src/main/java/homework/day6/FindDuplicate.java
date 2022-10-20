ed22d08b8285f4f17352575df379e50dc38a8066
Denis Davydyonok <denisdav06@gmail.com>
2022-10-20 18:31:09 +0300

    I have finished sub-task 2 for String task

---
+++ src/main/java/homework/day6/FindDuplicate.java
@@ -0,0 +1,44 @@
+package homework.day6;
+
+import java.util.ArrayList;
+
+public class FindDuplicate {
+
+    public void findDuplicate (String text) {
+
+//        1.2 сделать метод, который позволяет найти и вывести в консоль все дубликаты слов в строке текста, который принимает на вход в виде аргумента.
+
+        String[] words = text.split("[^A-Za-zА-Яа-я]+");
+        ArrayList<String> duplicate = new ArrayList<>();
+
+        for (String i : words) {
+                int counter = 0;
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(i)){
+                        counter++;
+                    }
+                    if (counter > 1) {
+                        if (!duplicate.contains(i)) {
+                            System.out.println(i);
+                            duplicate.add(i);
+                        }
+                        break;
+                    }
+
+            }
+        }
+
+        if (duplicate.isEmpty()) {
+            System.out.println("Текст не содержит дублирующихся слов");
+        }
+
+
+
+//        if (text.contains(keyWord)) {
+//            System.out.println("Текст содержит это слово");
+//        } else {
+//            System.out.println("Текст не содержит слово \"" + keyWord + "\"");
+//        }
+    }
+
+}
--- src/main/java/homework/day6/StringObjectRunner.java
+++ src/main/java/homework/day6/StringObjectRunner.java
@@ -5,8 +5,10 @@ public class StringObjectRunner {
     public static void main(String[] args) {
 
         PrintString testStringOne = new PrintString();
+        FindDuplicate testText = new FindDuplicate();
 
         testStringOne.printString("Вася пупкин");
+        testText.findDuplicate("Тестовый текст, для проверки дубликата! И если тут нет дубликата, то текст плохой((");
     }
 
 }
