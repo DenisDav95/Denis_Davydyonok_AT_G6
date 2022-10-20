30821eab61ef425e76ace39bbcc18173ec106eb2
Denis Davydyonok <denisdav06@gmail.com>
2022-10-20 16:18:17 +0300

    I have finished sub-task 1 for String task

---
+++ src/main/java/homework/day6/StringObjectRunner.java
@@ -0,0 +1,12 @@
+package homework.day6;
+
+public class StringObjectRunner {
+
+    public static void main(String[] args) {
+
+        PrintString testStringOne = new PrintString();
+
+        testStringOne.printString("Вася пупкин");
+    }
+
+}
