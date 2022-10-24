27af17f300be6ba35dfb833655123fd881ed0af3
Denis Davydyonok <denisdav06@gmail.com>
2022-10-24 19:26:00 +0300

    I have finished sub-task 2

---
+++ src/main/java/homework/day8/Figures.java
@@ -0,0 +1,43 @@
+package homework.day8;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Figures {
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> figures = new ArrayList<>(Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс"));
+        BufferedWriter logs = new BufferedWriter(new FileWriter("figures.txt"));
+
+        for (String i : figures) {
+            logs.newLine();
+            logs.write("-" + i);
+        }
+        logs.close();
+
+        int counter = 0;
+        for (String i : figures) {
+            if ((!i.contains("и"))) {
+                counter++;
+            }
+        }
+        System.out.println(counter + " фигур не содержат в стовем названии букву 'и'");
+
+        for (int i = 0; i < figures.size(); i++) {
+            System.out.printf(figures.get(i) + " ");
+        }
+        System.out.println();
+
+        figures.add(2, "Треугольник");
+
+        for (String i : figures) {
+           System.out.printf(i + " ");
+        }
+    }
+
+}
