f7231d7d84d11fc6490c3a0f475bf314b2d89d2a
Denis Davydyonok <denisdav06@gmail.com>
2022-11-09 23:43:30 +0300

    I have finished sub-tasks 1-2

---
+++ src/main/java/homework/day12/Hedgehog.java
@@ -0,0 +1,36 @@
+package homework.day12;
+
+public class Hedgehog {
+
+    private String pattern = "Hedgehog <n>";
+    private String name;
+
+    public Hedgehog(int number) {
+        this.name = "Hedgehog" + " " + number;
+    }
+
+    public void tellMe() {
+        System.out.println("I am " + getName() + " !");
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/homework/day12/Mouse.java
@@ -0,0 +1,36 @@
+package homework.day12;
+
+public class Mouse {
+
+    private String pattern = "Mouse <n>";
+    private String name;
+
+    public Mouse(int number) {
+        this.name = "Mouse" + " " + number;
+    }
+
+    public void peep() {
+        System.out.println(getName() + " PEEP!");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
